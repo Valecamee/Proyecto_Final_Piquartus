a8991c44510a1b1f6ae81116fb7792551b2c82c6
María Valentina Carvajal <maria.valentina.carvajal@correounivalle.edu.co>
2023-07-11 16:51:32 -0500

    sensor de luz

---
+++ Sistema_IoT.py
@@ -0,0 +1,15 @@
+import grovepi
+import csv
+import numpy as np
+from grovepi import *
+from grove_rgb_lcd import *
+from time import sleep
+from math import isnan
+import datetime
+
+button = 2
+dht_sensor_port = 3
+dht_sensor_type = 0 
+
+button_status = digitalRead(button)
+light_intensity = grovepi.analogRead(light_sensor)
