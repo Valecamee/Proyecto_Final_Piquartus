561373b228bafa5c2311186694d727e64df1c925
María Valentina Carvajal <maria.valentina.carvajal@correounivalle.edu.co>
2023-07-11 17:13:41 -0500

    Sensor temp, humedad

--- Sistema_IoT.py
+++ Sistema_IoT.py
@@ -10,6 +10,17 @@ import datetime
 button = 2
 dht_sensor_port = 3
 dht_sensor_type = 0 
+light_sensor = 1
 
+
+p = int(grovepi.analogRead(potentiometer)/204.5)
+now = datetime.datetime.now()
+formatted_date = now.strftime("%d/%m/%y/%H:%M:%S")
+        
 button_status = digitalRead(button)
 light_intensity = grovepi.analogRead(light_sensor)
+# check if we have nans
+# if so, then raise a type error exception
+[ temp,hum ] = dht(dht_sensor_port,dht_sensor_type)
+print("temp =", temp, "C\thumidity =", hum,"%")
+print(p)
